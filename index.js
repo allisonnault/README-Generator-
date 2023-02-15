2b3b770fa4b86bac6fa4fad181e8ef64ae960239
allisonnault <allisonrnault@gmail.com>
2023-02-15 16:08:41 -0600

    added working links to readme

--- index.js
+++ index.js
@@ -2,13 +2,44 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions;
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'project',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'gitHub',
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repository?",
+        name: 'repo',
+    },
+    {
+        type: 'input',
+        message: 'Enter a description of your project',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        default: 'npm i',
+        name: 'instal',
+    },
+    
+])
+.then((response) =>
+console.log(response));
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
---
+++ test.md
@@ -0,0 +1,41 @@
+# ${project}
+
+## Description
+${description}
+
+## Table of Contents
+
+[Installation](#installation)
+
+[Usage](#usage)
+
+[License](#license)
+
+[Contributing](#contributing)
+
+[Tests](#tests)
+
+[Questions](#questions)
+
+<a name="installation"></a>
+## Installation
+
+${instal}
+
+<a name="usage"></a>
+## Usage
+
+${usage}
+
+<a name="license"></a>
+## License
+
+<a name="contributing"></a>
+## Contributing
+
+<a name="tests"></a>
+## Tests
+
+<a name="questions"></a>
+## Questions
+alkdjfalkdjflaskjdfalsdkjf
